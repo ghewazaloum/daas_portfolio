c918ab42132d3465dd332d94e944ad03cfce7e56
ghewazaloum <ghewazaloum@gmail.com>
2024-02-02 12:41:02 +0300

    changes

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,5 +1,6 @@
 import './Information.css'
 import {Button,UnFocusedButton} from '../../components/index';
+import CV from '../../CV.pdf'
 function Information (){
     return(
         <div className="information">
@@ -7,7 +8,7 @@ function Information (){
             <h1> Daas Hatweek </h1>
             <p> Software engineer | Back end Django developer | Python developer.</p>
             <div className='Buttons'>
-            <Button>Download CV</Button>
+            <a href={CV}  download="CV-PDF-document" target="_blank" rel="noreferrer"><Button>Download CV</Button></a>
             <UnFocusedButton>contact me</UnFocusedButton>
             </div>
         </div>
